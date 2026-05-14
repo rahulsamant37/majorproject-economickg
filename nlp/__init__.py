1e5b72433ac8b1d65281e61b5521b771115159a5
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-05-14 08:44:53 +0530

    chore: add nlp package init

---
+++ nlp/__init__.py
@@ -0,0 +1,4 @@
+from nlp.entity_extractor import extract_entities, Entity
+from nlp.relationship_extractor import extract_relationships, Relationship
+
+__all__ = ["extract_entities", "Entity", "extract_relationships", "Relationship"]
